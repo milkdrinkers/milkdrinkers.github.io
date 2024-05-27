13838205df14bccdd1b0b83cb64456f557fe06d6
darksaid98 <darksaid9889@gmail.com>
2024-05-27 21:07:17 +0000

    Deploying to gh-pages from @ milkdrinkers/milkdrinkers.github.io@476c1163f8de7d89e8bf581180acf3d4ed61d17a 🚀

---
+++ _next/static/chunks/pages/_app-a90f6452921025cb.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[888],{1110:function(_,n,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return e(1824)}])}},function(_){var __webpack_exec__=function(n){return _(_.s=n)};_.O(0,[774,179],function(){return __webpack_exec__(1110),__webpack_exec__(4705)}),_N_E=_.O()}]);
