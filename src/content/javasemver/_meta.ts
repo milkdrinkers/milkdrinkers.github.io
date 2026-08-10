4f0fb305f28945fe94819b9204808d80d5b5b076
darksaid98 <darksaid9889@gmail.com>
2026-08-10 17:13:24 +0200

    feat: update/flesh out java semver documentation

--- src/content/javasemver/_meta.ts
+++ src/content/javasemver/_meta.ts
@@ -5,9 +5,12 @@ export default {
     },
     index: "Introduction",
     installation: "Installation",
+    migrating: "Migrating (1.X.X To 2.X.X)",
     "-- usage": {
         type: "separator",
         title: "Usage",
     },
-    usage: "Usage"
+    usage: "Versions",
+    ranges: "Ranges",
+    increment: "Incrementing & Diffing"
 }
